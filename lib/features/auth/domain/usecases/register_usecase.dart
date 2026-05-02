d6c0322797eaa7a68eafb7bcdcfe20f52b43aa14
EsraaMostafa22 <em61575@gmail.com>
2026-05-02 23:53:48 +0200

    fill auth entities and usecases

--- lib/features/auth/domain/usecases/register_usecase.dart
+++ lib/features/auth/domain/usecases/register_usecase.dart
@@ -15,22 +15,4 @@ class RegisterUseCase {
       userType: userType,
     );
   }
-}// domain/usecases/register_usecase.dart
-class RegisterUseCase {
-  final AuthRepository repository;
-  RegisterUseCase(this.repository);
-
-  Future<Either<Failure, User>> call({
-    required String email,
-    required String password,
-    required String name,
-    required String userType,
-  }) {
-    return repository.registerWithEmail(
-      email: email,
-      password: password,
-      name: name,
-      userType: userType,
-    );
-  }
 }
