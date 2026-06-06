d18ec1eb8fac4c7bc0d4994b190461191e61fea6
Mustafa Muhamed <mustafa3muhamed99>
2026-06-06 06:05:25 +0300

    add freelancer account routes to app router

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -4,6 +4,22 @@ import 'package:dipe_freelance/features/auth/presentation/views/signup_screen.da
 import 'package:dipe_freelance/features/freelance_dashboard/presentation/views/freelance_dashboard_view.dart';
 import 'package:dipe_freelance/features/user_dashboard/presentation/views/user_dashboard_view.dart';
 import 'package:dipe_freelance/features/on_boarding/presentation/screen/on_boarding_screen.dart';
+import 'package:dipe_freelance/features/freelancer_account/presentation/views/analysitcs_view.dart';
+import 'package:dipe_freelance/features/freelancer_account/presentation/views/finish_project_view.dart';
+import 'package:dipe_freelance/features/freelancer_account/presentation/views/funds_view.dart';
+import 'package:dipe_freelance/features/freelancer_account/presentation/views/home_view.dart';
+import 'package:dipe_freelance/features/freelancer_account/presentation/views/invite_recieved_view.dart';
+import 'package:dipe_freelance/features/freelancer_account/presentation/views/job_detalis.dart';
+import 'package:dipe_freelance/features/freelancer_account/presentation/views/massege_chat_view.dart';
+import 'package:dipe_freelance/features/freelancer_account/presentation/views/payment_view.dart';
+import 'package:dipe_freelance/features/freelancer_account/presentation/views/profile_view.dart';
+import 'package:dipe_freelance/features/freelancer_account/presentation/views/project_workspace_view.dart';
+import 'package:dipe_freelance/features/freelancer_account/presentation/views/projects_view.dart';
+import 'package:dipe_freelance/features/freelancer_account/presentation/views/proposal_send_view.dart';
+import 'package:dipe_freelance/features/freelancer_account/presentation/views/rate_client_view.dart';
+import 'package:dipe_freelance/features/freelancer_account/presentation/views/submit_proposal_view.dart';
+import 'package:dipe_freelance/features/freelancer_account/presentation/views/submit_work_view.dart';
+import 'package:dipe_freelance/features/freelancer_account/presentation/views/work_approved_view.dart';
 import 'package:go_router/go_router.dart';
 
 class AppRouter {
@@ -29,6 +45,70 @@ class AppRouter {
       GoRoute(
         path: AppRoutes.userDashboard,
         builder: (context, state) => const UserDashboardView(),
+      ),
+      GoRoute(
+        path: AppRoutes.analytics,
+        builder: (context, state) => const AnalysitcsView(),
+      ),
+      GoRoute(
+        path: AppRoutes.finishProject,
+        builder: (context, state) => const FinishProjectView(),
+      ),
+      GoRoute(
+        path: AppRoutes.funds,
+        builder: (context, state) => const FundsView(),
+      ),
+      GoRoute(
+        path: AppRoutes.home,
+        builder: (context, state) => const HomeView(),
+      ),
+      GoRoute(
+        path: AppRoutes.inviteReceived,
+        builder: (context, state) => const InviteRecievedView(),
+      ),
+      GoRoute(
+        path: AppRoutes.jobDetails,
+        builder: (context, state) => const JobDetailsView(),
+      ),
+      GoRoute(
+        path: AppRoutes.messageChat,
+        builder: (context, state) => const MessageChatView(),
+      ),
+      GoRoute(
+        path: AppRoutes.payment,
+        builder: (context, state) => const PaymentView(),
+      ),
+      GoRoute(
+        path: AppRoutes.profile,
+        builder: (context, state) => const FreelancerProfileView(),
+      ),
+      GoRoute(
+        path: AppRoutes.projectWorkspace,
+        builder: (context, state) => const ProjectWorkspaceView(),
+      ),
+      GoRoute(
+        path: AppRoutes.projects,
+        builder: (context, state) => const ProjectsView(),
+      ),
+      GoRoute(
+        path: AppRoutes.proposalSend,
+        builder: (context, state) => const ProposalSendView(),
+      ),
+      GoRoute(
+        path: AppRoutes.rateClient,
+        builder: (context, state) => const RateClientView(),
+      ),
+      GoRoute(
+        path: AppRoutes.submitProposal,
+        builder: (context, state) => const SubmitProposalView(),
+      ),
+      GoRoute(
+        path: AppRoutes.submitWork,
+        builder: (context, state) => const SubmitWorkView(),
+      ),
+      GoRoute(
+        path: AppRoutes.workApproved,
+        builder: (context, state) => const WorkApprovedView(),
       ),
     ],
   );
--- lib/core/router/app_routes.dart
+++ lib/core/router/app_routes.dart
@@ -8,4 +8,20 @@ class AppRoutes {
   static const String signup = '/signup';
   static const String freelanceDashboard = '/freelanceDashboard';
   static const String userDashboard = '/userDashboard';
+  static const String analytics = '/analytics';
+  static const String finishProject = '/finishProject';
+  static const String funds = '/funds';
+  static const String home = '/home';
+  static const String inviteReceived = '/inviteReceived';
+  static const String jobDetails = '/jobDetails';
+  static const String messageChat = '/messageChat';
+  static const String payment = '/payment';
+  static const String profile = '/profile';
+  static const String projectWorkspace = '/projectWorkspace';
+  static const String projects = '/projects';
+  static const String proposalSend = '/proposalSend';
+  static const String rateClient = '/rateClient';
+  static const String submitProposal = '/submitProposal';
+  static const String submitWork = '/submitWork';
+  static const String workApproved = '/workApproved';
 }
